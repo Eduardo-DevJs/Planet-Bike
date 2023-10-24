5ae1bcbb324c2a3c4b73e52c9fe95f01e2090e87
Kieper <kieperfmatheus@gmail.com>
2023-10-24 16:47:20 -0300

    dao e model

---
+++ src/main/java/dao/UsuarioDAO.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import conexao.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import model.UsuarioModel;
+
+/**
+ *
+ * @author mferreira
+ */
+public class UsuarioDAO {
+    Connection con = null;
+    PreparedStatement pstm = null;
+    
+    public ResultSet AutenticarLoginAdmin(UsuarioModel user){
+        String sql = "select * from admin where login = ?, senha = ?";
+        con = Conexao.createConnection();
+        try {
+            pstm = con.prepareStatement(sql);
+            pstm.setString(1, user.getLogin());
+            pstm.setString(2,user.getSenha());
+            ResultSet rset = pstm.executeQuery();
+            return rset;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro Login Admin:"+e);
+            return null;
+        }   
+    }
+    
+    public ResultSet AutenticarLoginFuncionario(UsuarioModel user){
+        String sql = "select * from funcionario where login = ?, senha = ?";
+        con = Conexao.createConnection();
+        try {
+            pstm = con.prepareStatement(sql);
+            pstm.setString(1, user.getLogin());
+            pstm.setString(2,user.getSenha());
+            ResultSet rset = pstm.executeQuery();
+            return rset;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro Login Admin:"+e);
+            return null;
+        }   
+    }
+}
---
+++ src/main/java/model/UsuarioModel.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author mferreira
+ */
+public class UsuarioModel {
+    int id;
+    String login;
+    String senha;
+
+    public UsuarioModel() {
+    }
+
+    public UsuarioModel(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
