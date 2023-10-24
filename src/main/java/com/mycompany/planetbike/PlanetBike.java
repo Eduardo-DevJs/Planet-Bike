160ab46e5dd151cf41f6c6f6f230741be4d92007
Eduardo de Oliveira Liria <110121383+Eduardo-DevJs@users.noreply.github.com>
2023-10-24 09:00:31 -0300

    first commit

---
+++ src/main/java/com/mycompany/planetbike/PlanetBike.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.planetbike;
+
+/**
+ *
+ * @author eliria
+ */
+public class PlanetBike {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
