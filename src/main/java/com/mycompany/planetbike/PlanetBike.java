9f70abe29ecbde20c19fecbba7ad22bd0eb0267a
KieperMF <130458583+KieperMF@users.noreply.github.com>
2023-10-24 09:13:15 -0300

    oi dudu

--- src/main/java/com/mycompany/planetbike/PlanetBike.java
+++ src/main/java/com/mycompany/planetbike/PlanetBike.java
@@ -11,6 +11,6 @@ package com.mycompany.planetbike;
 public class PlanetBike {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("oi duduuuuuuuuuuuuuuuuuuuuu");
     }
 }
