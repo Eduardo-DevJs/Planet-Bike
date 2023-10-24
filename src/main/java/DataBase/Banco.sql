44cb24158add0a209def36b4ac2241ad74c2bb74
Kieper <kieperfmatheus@gmail.com>
2023-10-24 15:29:33 -0300

    Banco de Dados

--- src/main/java/DataBase/Banco.sql
+++ src/main/java/DataBase/Banco.sql
@@ -4,6 +4,7 @@ use bd_planetbike;
 create table funcionario(
 funcionarioId int(11) primary key auto_increment not null,
 nome varchar(200) not null,
-senha varchar(50) not null
+senha varchar(50) not null,
+perfil varchar(50) not null
 );
 
